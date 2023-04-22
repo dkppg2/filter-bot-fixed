5e99e80f36bd0629150f9f21f1d0acf7c742367b
dkppg2 <117648465+dkppg2@users.noreply.github.com>
2023-04-22 18:25:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/dkppg2/url-auto-delete-shortener-bot/tree/Shareus /Eva
+  git clone https://github.com/dkppg2/url-auto-delete-shortener-bot.git /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
